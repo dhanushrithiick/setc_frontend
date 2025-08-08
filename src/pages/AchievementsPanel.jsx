2568f947c746c74a0684fc50d0e70977f2d0f0dc
dhanushrithiick <dhanushrithiick@gmail.com>
2025-08-08 20:49:29 +0530

    Update AchievementsPanel.jsx

--- src/pages/AchievementsPanel.jsx
+++ src/pages/AchievementsPanel.jsx
@@ -25,7 +25,7 @@ const AchievementsPanel = () => {
 
 const handleDelete = async (id) => {
   try {
-    const response = await fetch(`https://setc-backend.onrender.com/${id}`, {
+    const response = await fetch(`https://setc-backend.onrender.com/achievement/${id}`, {
       method: 'DELETE',
     });
 
